5bbd27299739de0cb04360ab34e4b0d799423f8d
Edouard Zinnoussou <42722304+Edouardsnipeur@users.noreply.github.com>
2022-10-04 21:00:28 +0000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+TOKEN = '5418296339:AAF0rzZ2INQvdXRR4hYeInt4X2YbEj4xaPo'
+
+# This function replies with 'Hello <user.first_name>'
+def hello(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(''.join([l.upper() if i % 2 == 0 else l for (i, l) in enumerate(update.message.text)]))
+
+# Insert your token here
+updater = Updater(TOKEN)
+
+# Make the hello command run the hello function
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+# Connect to Telegram and wait for messages
+updater.start_polling()
+
+# Keep the program running until interrupted
+updater.idle()
+
